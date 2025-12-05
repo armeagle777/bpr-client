879fe284856771f86642e243c681b65e3de2c614
Tigran Yeranyan <tigran.e@purlin.com>
2025-12-06 03:18:59 +0400

    douplicate address modified exclude

--- src/components/search/helpers.js
+++ src/components/search/helpers.js
@@ -1,11 +1,50 @@
+const getRaAddressSignature = (raAddress = {}) => ({
+  location_code: raAddress.location_code || 'NO_LOCATION',
+  region: raAddress.region || 'NO_REGION',
+  community: raAddress.community || 'NO_COMMUNITY',
+  street: raAddress.street || 'NO_STREET',
+  building: raAddress.building || 'NO_BUILDING',
+  building_type: raAddress.building_type || 'NO_BUILDING_TYPE',
+  apartment: raAddress.apartment || 'NO_APARTMENT',
+});
+
+const getFcAddressSignature = (fcAddress = {}) => ({
+  country_code:
+    fcAddress.foreign_country?.code ||
+    fcAddress.foreign_country?.short_name ||
+    fcAddress.foreign_country?.name ||
+    'NO_COUNTRY',
+  region: fcAddress.foreign_region || 'NO_FOREIGN_REGION',
+  community: fcAddress.foreign_community || 'NO_FOREIGN_COMMUNITY',
+  city: fcAddress.foreign_city || 'NO_FOREIGN_CITY',
+  street: fcAddress.foreign_street || 'NO_FOREIGN_STREET',
+  building: fcAddress.foreign_building || 'NO_FOREIGN_BUILDING',
+  apartment: fcAddress.foreign_apartment || 'NO_FOREIGN_APARTMENT',
+});
+
+const buildAddressSignature = (address = {}) => {
+  const regData = address.registration_data || {};
+  const regAddress = address.registration_address || {};
+  const raAddress = regAddress.ra_address;
+  const fcAddress = regAddress.fc_address;
+
+  const baseSignature = {
+    status: regData.regist_status || 'REG_STATUS_UNKNOWN',
+    type: regData.regist_type || 'REG_TYPE_UNKNOWN',
+  };
+
+  const addressSignature = raAddress
+    ? { mode: 'RA', ...getRaAddressSignature(raAddress) }
+    : { mode: 'FC', ...getFcAddressSignature(fcAddress || {}) };
+
+  return JSON.stringify({ ...baseSignature, ...addressSignature });
+};
+
 export const dedupeAddressesByStatusAndCode = (addresses = []) => {
   const seen = new Set();
 
   return addresses.filter((address = {}) => {
-    const status = address.registration_data?.regist_status || 'REG_STATUS_UNKNOWN';
-    const locationCode = address.registration_address?.ra_address?.location_code || 'NO_LOCATION';
-
-    const key = `${status}-${locationCode}`;
+    const key = buildAddressSignature(address);
 
     if (seen.has(key)) return false;
 
